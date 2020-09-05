f8b5738c2c36ecb0ef445e4871f183fe78b2a952
syum-akhlaque <arssyum@gmail.com>
2020-09-06 04:50:26 +0600

    content show in blog page

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -1,37 +1,32 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
-const useStyles = makeStyles({
-    root: {
-      maxWidth: 345,
-    },
-    media: {
-      height: 140,
-    },
-  });
-  
-
+import Grid from '@material-ui/core/Grid'
+import './blog.css';
 const Blog = (props) => {
 
     const {title, body} = props.article;
-    const classes = useStyles();
+    const sliceTitle = title.slice(0, 15)
+    const sliceBody = body.slice(0, 150)
+  
     
     return ( 
-       <div className = "newsCard"> 
-            <Card className={classes.root} >
+       <div className = 'singlePost'> 
+         <Grid item xs={6} sm={3}>
+            {/* <Card className={classes.root} > */}
+            <Card  >
+
             <CardActionArea>
                 <CardContent>
                 <Typography gutterBottom variant="h5" component="h2">
-                    {title}
+                    {sliceTitle}
                 </Typography>
                 <Typography variant="body2" color="textSecondary" component="p">
-                {body}
+                {sliceBody}
                 </Typography>
                 </CardContent>
             </CardActionArea>
@@ -41,6 +36,7 @@ const Blog = (props) => {
                 </Button>
             </CardActions>
             </Card> 
+          </Grid>
         </div>    
     );
 };
