54c2e496ba30490f7a7fe3b5aa457ae1fc9f6435
syum-akhlaque <arssyum@gmail.com>
2020-11-27 16:11:39 +0600

    check netlify

---
+++ build/precache-manifest.abea15b7e4a5cd2fc9abef79de74ee0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e543c761cecea59efae60e2a3bda20c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5311c75117cb981ad081",
+    "url": "/static/css/main.d241c38d.chunk.css"
+  },
+  {
+    "revision": "8b5e75c2f9b2ab9d00d8",
+    "url": "/static/js/2.b19c11fd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b19c11fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5311c75117cb981ad081",
+    "url": "/static/js/main.789630ea.chunk.js"
+  },
+  {
+    "revision": "9b6223ceb35aee5c6d95",
+    "url": "/static/js/runtime-main.c807aca6.js"
+  }
+]);
